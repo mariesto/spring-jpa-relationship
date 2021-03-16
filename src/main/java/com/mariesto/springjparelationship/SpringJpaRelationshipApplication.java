8f09314d82c04e9f39750115028581ac253dcd66
mariesto <sitinjakamendo@gmail.com>
2021-03-16 22:33:47 +0700

    Initial

---
+++ src/main/java/com/mariesto/springjparelationship/SpringJpaRelationshipApplication.java
@@ -0,0 +1,13 @@
+package com.mariesto.springjparelationship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaRelationshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaRelationshipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mariesto/springjparelationship/SpringJpaRelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariesto.springjparelationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaRelationshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
